dd53909979b2a33d70988748b4edd257a5227492
Wojtek <Wojtek@DESKTOP-4FI9M6M>
2023-04-24 19:47:49 +0200

    added human ability

--- Human.cpp
+++ Human.cpp
@@ -8,6 +8,10 @@
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
+#define ABILITY_STRENGTH 10
+#define BASE_STRENGTH 5
+#define ABILITY_COOLDOWN 5
+
 Human::Human(World& world) : Animal(world, true) {
 	world.board[0][0] = this;
 	this->posX = 0;
@@ -21,30 +25,53 @@ void Human::draw() {
 	printf("H");
 }
 
+void Human::handleInput() {
+	int newInput2 = _getch();
+	switch (newInput2) {
+	case KEY_UP:
+		this->posY--;
+		break;
+	case KEY_DOWN:
+		this->posY++;
+		break;
+	case KEY_LEFT:
+		this->posX--;
+		break;
+	case KEY_RIGHT:
+		this->posX++;
+		break;
+	}
+}
+
 void Human::act() {
 	//Handle user input
 	int newInput, newInput2;
 	this->beforeMoveX = this->posX;
 	this->beforeMoveY = this->posY;
-	newInput = _getch();
-	if (newInput == 0 || newInput == 224) {
-		newInput2 = _getch();
-		switch (newInput2) {
-		case KEY_UP:
-			this->posY--;
-			break;
-		case KEY_DOWN:
-			this->posY++;
-			break;
-		case KEY_LEFT:
-			this->posX--;
-			break;
-		case KEY_RIGHT:
-			this->posX++;
-			break;
+
+	if (this->isAbilityTurnedOn) {
+		this->abilityCooldown = ABILITY_COOLDOWN;
+		if (this->strength == BASE_STRENGTH) {
+			this->isAbilityTurnedOn = false;
+		}
+		else this->strength--;
+	}
+	else {
+		if (this->abilityCooldown > 0) {
+			this->abilityCooldown--;
 		}
 	}
-		
+
+	while (this->posX == this->beforeMoveX && this->posY == this->beforeMoveY) {
+		newInput = _getch();
+
+		if (newInput == 0 || newInput == 224) {
+			handleInput();
+		}
+		else if (newInput == 'v') {
+			Human::abillity();
+		}
+	}
 
 	//Check if in board
 
@@ -56,6 +83,15 @@ void Human::act() {
 
 	if (this->world.board[this->posX][this->posY] != nullptr && this->posX != this->beforeMoveX && this->posY != this->beforeMoveY) collision();
 
+}
+
+void Human::abillity() {
+	if (this->abilityCooldown == 0) {
+		this->isAbilityTurnedOn = true;
+		this->strength = ABILITY_STRENGTH;
+		this->world.logs.push_front("You have activated you super ability! ");
+
+	}
 }
 
 void Human::collision() {
--- Human.h
+++ Human.h
@@ -3,11 +3,16 @@
 #include "output.h"
 
 class Human : public Animal {
+private:
+	bool isAbilityTurnedOn = false;
+	int abilityCooldown = 0;
 public:
 	Human(World& world);
 	Human* clone() override;
+	void handleInput();
 	void act();
 	void draw();
 	void collision();
+	void abillity();
 	~Human();
 };
